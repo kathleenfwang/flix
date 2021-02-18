ee6a261fecd37fde42877d179eabd6de1c5238c2
Kathleen Wang <kathleenchibimaru@gmail.com>
2021-02-17 16:57:17 -0800

    add async http client and make get call to 'now playing url'

--- app/src/main/java/com/kathleenwang/flixster/MainActivity.java
+++ app/src/main/java/com/kathleenwang/flixster/MainActivity.java
@@ -3,12 +3,35 @@ package com.kathleenwang.flixster;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONObject;
+
+import okhttp3.Headers;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String NOW_PLAYING_URL = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(NOW_PLAYING_URL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
     }
 }
